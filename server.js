6c3c96913566ee91b705599583dac27b43b097d9
Vajrayini <vajrayinivaji@gmail.com>
2026-02-13 15:01:42 +0530

    Fixed Atlas password encoding

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const axios = require("axios");
 
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
